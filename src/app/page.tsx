0cd7afb473a799d4a90b94e9b7977ac7b505890a
Rudranarayan Ratha <138389474+Rudranarayan12@users.noreply.github.com>
2024-10-26 10:52:30 +0530

    error code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,12 @@
 "use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 
 const page = () => {
   return (
     <div className="h-full flex justify-center items-center flex-col">
       <Button className="  p-9 ">hello</Button>
-      <AlertDialog>
-        <AlertDialogTrigger>Open</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <h1>Error code are injected succesfully</h1>
     </div>
   );
 };
